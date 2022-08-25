a2c8dd9981c23e9ab3430c6f862c91c1e666fb47
Marximus Maximus <marximus@marximus.com>
2022-08-25 15:19:53 -0700

    some cleanup for make_release.py

--- make_release.py
+++ make_release.py
@@ -2,6 +2,7 @@
 
 import json
 import subprocess
+import sys
 
 name = "thermal-solar-power"
 
@@ -10,4 +11,6 @@ text = f.read()
 data = json.loads(text)
 version = data["version"]
 
-subprocess.run(["zip", "-vr", f"{name}_{version}.zip", f"{name}"])
+res = subprocess.run(["zip", "-vr", f"{name}_{version}.zip", f"{name}"])
+
+sys.exit(res.returncode)
